d79dc1810318e8391536aae6ae5f6680d747be4f
swastikCommits <textswastik.alt@gmail.com>
2025-06-28 20:54:03 +0530

    init

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,7 +1,19 @@
-export function GET() {
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client"
+const client = new PrismaClient();
+
+export async function POST(req: NextResponse) {
+
+    const body = await req.json();
+    await client.user.create({
+        data:{
+            username: body.username,
+            password: body.password
+        }
+    })
+
   return Response.json({
-    username: 'bebu',
-    email: 'bebu@gmail.com',
+    msg: "You are logged in"
   });
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,8 @@
-import axios from 'axios';
-
-async function getUserDetails() {
-  await new Promise((r)=>setTimeout(r,1000));
-  const response = await axios.get(" http://localhost:3000/api/user");
-  return response.data;
-}
 
 export default async function Home() {
-  const userDetails = await getUserDetails();
-
   return (
-    <div className="flex flex-col justify-center h-screen">
-      <div className="flex justify-center">
-        <div className="border p-8 rounded">
-          <div>
-            Name: {userDetails?.name}
-          </div>
-          {userDetails?.email}
-        </div>
-      </div>
+    <div>
+      hi there  
     </div>
   )
 }
